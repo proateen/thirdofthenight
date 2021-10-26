0e709456411d2838ed062907fa75d6eaa2328724
Ahmed Samadzadeh <52085315+proateen@users.noreply.github.com>
2021-10-26 12:28:44 +0100

    Added midnight time and disclaimer

--- script.js
+++ script.js
@@ -1,7 +1,24 @@
+const modal = document.querySelector(".modal");
+const overlay = document.querySelector(".overlay");
+const btnCloseModal = document.querySelector(".close-modal");
+const btnOpenModal = document.querySelector(".disclaimer");
+const openModal = () => {
+  modal.classList.remove("hidden");
+  overlay.classList.remove("hidden");
+};
+const closeModal = () => {
+  modal.classList.add("hidden");
+  overlay.classList.add("hidden");
+};
+let lastThirdOfTheNightTimeStart = document.getElementById("third");
+let midnightStart = document.getElementById("midnight");
+let difference = 0;
 let thirdOfTheDifference = 0;
-let tahajjud = 0;
+let halfOfTheDifference = 0;
+let lastThirdOfTheNight = 0;
+let midnight = 0;
 
-let calculateTahajjud = () => {
+let calculateTimes = () => {
   let maghrib = document.getElementById("maghrib").value;
   let fajr = document.getElementById("fajr").value;
   if (maghrib == "" || fajr == "") {
@@ -11,14 +28,32 @@ let calculateTahajjud = () => {
   } else {
     maghrib = dayjs(`2000-01-01 ${document.getElementById("maghrib").value}`);
     fajr = dayjs(`2000-01-02 ${document.getElementById("fajr").value}`);
-    thirdOfTheDifference = fajr.diff(maghrib) / 3;
-    tahajjud = fajr.subtract(thirdOfTheDifference, "milliseconds");
-    document.getElementById("third").innerHTML = tahajjud.format("HH:mm");
-    document.getElementById("third").style.visibility = "visible";
+    difference = fajr.diff(maghrib);
+    thirdOfTheDifference = difference / 3;
+    halfOfTheDifference = difference / 2;
+    lastThirdOfTheNight = fajr.subtract(thirdOfTheDifference, "milliseconds").format("HH:mm");
+    midnight = fajr.subtract(halfOfTheDifference, "milliseconds").format("HH:mm");
+    lastThirdOfTheNightTimeStart.innerHTML = lastThirdOfTheNight;
+    lastThirdOfTheNightTimeStart.style.visibility = "visible";
+    midnightStart.innerHTML = midnight;
+    midnightStart.style.visibility = "visible";
     document.getElementById("disclaimer").style.visibility = "visible";
+    document.querySelector(".userthird").innerHTML = lastThirdOfTheNight;
+    document.querySelector(".usermidnight").innerHTML = midnight;
   }
 };
 
 document
   .querySelector(".calculate")
-  .addEventListener("click", calculateTahajjud);
+  .addEventListener("click", calculateTimes);
+
+btnOpenModal.addEventListener("click", openModal);
+
+btnCloseModal.addEventListener("click", closeModal);
+overlay.addEventListener("click", closeModal);
+
+document.addEventListener("keydown", e => {
+  if (e.key === "Escape" && !modal.classList.contains("hidden")) {
+    closeModal();
+  };
+});
