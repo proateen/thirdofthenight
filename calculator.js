69c0dd4ee42f75fdf1e8692c429789322bc2b9f2
proateen <samadzadeh77+github@gmail.com>
2024-09-09 18:59:48 +0100

    Support all countries - If Maghrib is technically before Fajr on the 24-hour clock (i.e., Maghrib time occurs earlier than Fajr time), we set both Maghrib and Fajr on the same day (2000-01-01). This adjustment ensures accurate time difference calculations and avoids incorrect results. With this change, the calculator can handle scenarios in high-latitude regions where the Maghrib and Fajr times are very close and occur on the same day.

--- calculator.js
+++ calculator.js
@@ -2,14 +2,17 @@ const modal = document.querySelector(".modal");
 const overlay = document.querySelector(".overlay");
 const btnCloseModal = document.querySelector(".close-modal");
 const btnOpenModal = document.querySelector(".disclaimer");
+
 const openModal = () => {
   modal.classList.remove("hidden");
   overlay.classList.remove("hidden");
 };
+
 const closeModal = () => {
   modal.classList.add("hidden");
   overlay.classList.add("hidden");
 };
+
 let lastThirdOfTheNightTimeStart = document.getElementById("third");
 let midnightStart = document.getElementById("midnight");
 let difference = 0;
@@ -19,20 +22,28 @@ let lastThirdOfTheNight = 0;
 let midnight = 0;
 
 let calculateTimes = () => {
-  let maghrib = document.getElementById("maghrib").value;
-  let fajr = document.getElementById("fajr").value;
-  if (maghrib == "" || fajr == "") {
+  let maghribTime = document.getElementById("maghrib").value;
+  let fajrTime = document.getElementById("fajr").value;
+
+  if (maghribTime == "" || fajrTime == "") {
     window.alert(
       "Are you sure you have entered the times for Maghrib and Fajr correctly? Try again."
     );
   } else {
-    maghrib = dayjs(`2000-01-01 ${document.getElementById("maghrib").value}`);
-    fajr = dayjs(`2000-01-02 ${document.getElementById("fajr").value}`);
+    let maghrib = dayjs(`2000-01-01 ${maghribTime}`);
+    let fajr = dayjs(`2000-01-02 ${fajrTime}`);
+
+    if (maghrib.isBefore(dayjs(`2000-01-01 ${fajrTime}`))) {
+      fajr = dayjs(`2000-01-01 ${fajrTime}`);
+    }
+
     difference = fajr.diff(maghrib);
     thirdOfTheDifference = difference / 3;
     halfOfTheDifference = difference / 2;
+
     lastThirdOfTheNight = fajr.subtract(thirdOfTheDifference, "milliseconds").format("HH:mm");
     midnight = fajr.subtract(halfOfTheDifference, "milliseconds").format("HH:mm");
+
     lastThirdOfTheNightTimeStart.innerHTML = lastThirdOfTheNight;
     lastThirdOfTheNightTimeStart.style.visibility = "visible";
     midnightStart.innerHTML = midnight;
@@ -43,17 +54,12 @@ let calculateTimes = () => {
   }
 };
 
-document
-  .querySelector(".calculate")
-  .addEventListener("click", calculateTimes);
-
+document.querySelector(".calculate").addEventListener("click", calculateTimes);
 btnOpenModal.addEventListener("click", openModal);
-
 btnCloseModal.addEventListener("click", closeModal);
 overlay.addEventListener("click", closeModal);
-
-document.addEventListener("keydown", e => {
+document.addEventListener("keydown", (e) => {
   if (e.key === "Escape" && !modal.classList.contains("hidden")) {
     closeModal();
-  };
+  }
 });
